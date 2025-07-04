0586a63f9f72568f79d9f3c2681a6bff82c3f987
AfnanElsebaiy <102579326+AfnanElsebaiy@users.noreply.github.com>
2025-07-04 10:28:25 +0300

    afnan

---
+++ ReservationSystem.Domain/Enums/Enum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Domain.Enums
+{
+    public class Enum
+    {
+        public enum Result
+        {
+            Failed = 0,
+            Success = 1,
+            Exist = 2,
+            NoDataFound = 3,
+        }
+
+        public enum AlartType
+        {
+            information = 0,
+            warrning = 1,
+            error = 2,
+            success = 3
+        }
+        public enum AlartShow
+        {
+            note = 0,
+            popup = 1
+        }
+    }
+}
---
+++ ReservationSystem.Domain/Shared/ResponseResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ReservationSystem.Domain.Enums.Enum;
+
+namespace ReservationSystem.Domain.Shared
+{
+    public class ResponseResult
+    {
+        public Result Result { get; set; }
+#nullable enable
+        public object? DataCount { get; set; }
+        public object? Data { get; set; }
+        public Alart? Alart { get; set; }
+        public string? Note { get; set; } // notes for front
+        public int TotalCount { get; set; } // Count in Table
+        public string ErrorMessageAr { get; set; }
+        public string ErrorMessageEn { get; set; }
+        public double Total { get; set; }
+    }
+    public class Alart
+    {
+        public AlartType AlartType { get; set; } // alart type is the type of the alart danger,info,worning and success 
+        public AlartShow type { get; set; } // alart show for showing alart in notify or popup
+        public string MessageAr { get; set; }
+        public string MessageEn { get; set; }
+    }
+}
