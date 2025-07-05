a7c65e71cecc928d9c0c91050c887bf7eb1663c8
AfnanElsebaiy <102579326+AfnanElsebaiy@users.noreply.github.com>
2025-07-05 14:36:25 +0300

    afnan

---
+++ ReservationSystem.Application/DTOs/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Application.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public int Role { get; set; } // "Admin" or "User"
+    }
+}
