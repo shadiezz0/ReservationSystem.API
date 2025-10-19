73a471f8d05e755069770758041dc1b1c564379f
Afnan <afnan.elsebaiy@gmail.com>
2025-10-19 18:04:19 +0300

    afnan

---
+++ ReservationSystem.Infrastructure/Migrations/20251019150332_listItemTypeInItemTable.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservationSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class listItemTypeInItemTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_ItemTypes_ItemTypeId",
+                table: "Items");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_ItemTypeId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "ItemTypeId",
+                table: "Items");
+
+            migrationBuilder.CreateTable(
+                name: "ItemItemType",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    ItemTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemItemType", x => new { x.ItemId, x.ItemTypeId });
+                    table.ForeignKey(
+                        name: "FK_ItemItemType_ItemTypes_ItemTypeId",
+                        column: x => x.ItemTypeId,
+                        principalTable: "ItemTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemItemType_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_ItemTypeId",
+                table: "Reservations",
+                column: "ItemTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemItemType_ItemTypeId",
+                table: "ItemItemType",
+                column: "ItemTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_ItemTypes_ItemTypeId",
+                table: "Reservations",
+                column: "ItemTypeId",
+                principalTable: "ItemTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_ItemTypes_ItemTypeId",
+                table: "Reservations");
+
+            migrationBuilder.DropTable(
+                name: "ItemItemType");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_ItemTypeId",
+                table: "Reservations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ItemTypeId",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_ItemTypeId",
+                table: "Items",
+                column: "ItemTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_ItemTypes_ItemTypeId",
+                table: "Items",
+                column: "ItemTypeId",
+                principalTable: "ItemTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
